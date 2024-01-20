7937e1551306c86ba71f8578987baf7d831d4199
akarptra <rakario14@gmail.com.com>
2024-01-20 19:46:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,297 @@
+const API = "6e9e9bc3fd9bd9292b94faef813f7f08";
+
+const dayEl = document.querySelector(".default_day");
+const dateEl = document.querySelector(".default_date");
+const btnEl = document.querySelector(".btn_search");
+const inputEl = document.querySelector(".input_field");
+
+const iconsContainer = document.querySelector(".icons");
+const dayInfoEl = document.querySelector(".day_info");
+const listContentEl = document.querySelector(".list_content ul");
+const btnCurrentLocation = document.querySelector(".btn_current_location");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+// display the day
+const day = new Date();
+const dayName = days[day.getDay()];
+dayEl.textContent = dayName;
+
+// display date
+let month = day.toLocaleString("default", { month: "long" });
+let date = day.getDate();
+let year = day.getFullYear();
+
+console.log();
+dateEl.textContent = date + " " + month + " " + year;
+
+// add event
+btnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // Periksa nilai input
+  if (inputEl.value !== "") {
+    const searchQuery = inputEl.value;
+    inputEl.value = "";
+    
+    // Panggil fungsi pencarian berdasarkan nama kota
+    findLocation(searchQuery);
+  } else {
+    console.log("Please Enter City or Country Name");
+  }
+});
+
+btnCurrentLocation.addEventListener("click", getCurrentLocation);
+
+// Function untuk mendapatkan lokasi saat ini
+function getCurrentLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+
+async function findLocation(name) {
+  iconsContainer.innerHTML = "";
+  dayInfoEl.innerHTML = "";
+  listContentEl.innerHTML = "";
+  try {
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API}`;
+    const data = await fetch(API_URL);
+    const result = await data.json();
+    console.log(result);
+
+    if (result.cod !== "404") {
+      // display image content
+      const ImageContent = displayImageContent(result);
+
+      // display right side content
+      const rightSide = rightSideContent(result);
+
+      // forecast function
+      displayForeCast(result.coord.lat, result.coord.lon);
+
+      setTimeout(() => {
+        iconsContainer.insertAdjacentHTML("afterbegin", ImageContent);
+        iconsContainer.classList.add("fadeIn");
+        dayInfoEl.insertAdjacentHTML("afterbegin", rightSide);
+      }, 1500);
+    } else {
+      const message = `<h2 class="weather_temp">${result.cod}</h2>
+      <h3 class="cloudtxt">${result.message}</h3>`;
+      iconsContainer.insertAdjacentHTML("afterbegin", message);
+    }
+  } catch (error) {}
+}
+
+// display image content and temp
+function displayImageContent(data) {
+  return `<img src="https://openweathermap.org/img/wn/${
+    data.weather[0].icon
+  }@4x.png" alt="" />
+    <h2 class="weather_temp">${Math.round(data.main.temp - 275.15)}°C</h2>
+    <h3 class="cloudtxt">${data.weather[0].description}</h3>`;
+}
+
+// display the right side content
+function rightSideContent(result) {
+  return `<div class="content">
+          <p class="title">NAME</p>
+          <span class="value">${result.name}</span>
+        </div>
+        <div class="content">
+          <p class="title">TEMP</p>
+          <span class="value">${Math.round(result.main.temp - 275.15)}°C</span>
+        </div>
+        <div class="content">
+          <p class="title">HUMIDITY</p>
+          <span class="value">${result.main.humidity}%</span>
+        </div>
+        <div class="content">
+          <p class="title">WIND SPEED</p>
+          <span class="value">${result.wind.speed} Km/h</span>
+        </div>`;
+}
+
+async function displayForeCast(lat, long) {
+  const ForeCast_API = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${API}`;
+  const data = await fetch(ForeCast_API);
+  const result = await data.json();
+  // filter the forecast
+  const uniqeForeCastDays = [];
+  const daysForecast = result.list.filter((forecast) => {
+    const forecastDate = new Date(forecast.dt_txt).getDate();
+    if (!uniqeForeCastDays.includes(forecastDate)) {
+      return uniqeForeCastDays.push(forecastDate);
+    }
+  });
+  console.log(daysForecast);
+
+  daysForecast.forEach((content, indx) => {
+    if (indx <= 3) {
+      listContentEl.insertAdjacentHTML("afterbegin", forecast(content));
+    }
+  });
+}
+
+// forecast html element data
+function forecast(frContent) {
+  const day = new Date(frContent.dt_txt);
+  const dayName = days[day.getDay()];
+  const splitDay = dayName.split("", 3);
+  const joinDay = splitDay.join("");
+
+  // console.log(dayName);
+
+  return `<li>
+  <img src="https://openweathermap.org/img/wn/${
+    frContent.weather[0].icon
+  }@2x.png" />
+  <span>${joinDay}</span>
+  <span class="day_temp">${Math.round(frContent.main.temp - 275.15)}°C</span>
+</li>`;
+}
+
+// ...
+
+// Add event listener for the "Search" button
+btnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // check empty value
+  if (inputEl.value !== "") {
+      const Search = inputEl.value;
+      inputEl.value = "";
+      findLocation(Search);
+  } else {
+      console.log("Please Enter City or Country Name");
+  }
+});
+
+// Call getLocation function directly to get user's location automatically
+getLocation();
+
+// Function to get user's location
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+// Function to handle geolocation errors
+function showError(error) {
+  switch (error.code) {
+      case error.PERMISSION_DENIED:
+          console.log("User denied the request for Geolocation.");
+          break;
+      case error.POSITION_UNAVAILABLE:
+          console.log("Location information is unavailable.");
+          break;
+      case error.TIMEOUT:
+          console.log("The request to get user location timed out.");
+          break;
+      case error.UNKNOWN_ERROR:
+          console.log("An unknown error occurred.");
+          break;
+  }
+}
+
+// Function to display user's position
+function showPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  findLocationByCoordinates(latitude, longitude);
+}
+
+async function findLocationByCoordinates(latitude, longitude) {
+  iconsContainer.innerHTML = "";
+  dayInfoEl.innerHTML = "";
+  listContentEl.innerHTML = "";
+  try {
+      const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API}`;
+      const data = await fetch(API_URL);
+      const result = await data.json();
+
+      if (result.cod !== "404") {
+          // display image content
+          const ImageContent = displayImageContent(result);
+
+          // display right side content
+          const rightSide = rightSideContent(result);
+
+          // forecast function
+          displayForeCast(result.coord.lat, result.coord.lon);
+
+          // Change background based on location name
+          changeBackground(result.name);
+
+          setTimeout(() => {
+              iconsContainer.insertAdjacentHTML("afterbegin", ImageContent);
+              iconsContainer.classList.add("fadeIn");
+              dayInfoEl.insertAdjacentHTML("afterbegin", rightSide);
+          }, 1);
+      } else {
+          const message = `<h2 class="weather_temp">${result.cod}</h2>
+            <h3 class="cloudtxt">${result.message}</h3>`;
+          iconsContainer.insertAdjacentHTML("afterbegin", message);
+      }
+  } catch (error) {
+      console.error('Error fetching weather data:', error);
+  }
+}
+
+async function findLocation(name) {
+  iconsContainer.innerHTML = "";
+  dayInfoEl.innerHTML = "";
+  listContentEl.innerHTML = "";
+  try {
+      const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API}`;
+      const data = await fetch(API_URL);
+      const result = await data.json();
+
+      if (result.cod !== "404") {
+          // display image content
+          const ImageContent = displayImageContent(result);
+
+          // display right side content
+          const rightSide = rightSideContent(result);
+
+          // forecast function
+          displayForeCast(result.coord.lat, result.coord.lon);
+
+          // Change background based on location name
+          changeBackground(result.name);
+
+          setTimeout(() => {
+              iconsContainer.insertAdjacentHTML("afterbegin", ImageContent);
+              iconsContainer.classList.add("fadeIn");
+              dayInfoEl.insertAdjacentHTML("afterbegin", rightSide);
+          }, 1500);
+      } else {
+          const message = `<h2 class="weather_temp">${result.cod}</h2>
+            <h3 class="cloudtxt">${result.message}</h3>`;
+          iconsContainer.insertAdjacentHTML("afterbegin", message);
+      }
+  } catch (error) {
+      console.error('Error fetching weather data:', error);
+  }
+}
+
+// Function to change background based on location name
+function changeBackground(locationName) {
+  document.body.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${locationName}")`;
+}
+
